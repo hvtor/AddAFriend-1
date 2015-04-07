953b4f358b084b63df5c2303e20ddcef48916500
Hemant Torsekar <hvt@me.com>
2015-04-07 14:37:53 -0400

    Just saying hi.

--- AddAFriend/ViewController.m
+++ AddAFriend/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    // Hi Itay.  This is a comment from Hemant.
 }
 
 - (void)didReceiveMemoryWarning {
